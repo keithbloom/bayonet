98fbae6c2aab14d917021a06e8b00489c5899a01
Keith Bloom <keith.bloom@gmail.com>
2011-05-23 22:24:51 +0100

    Extracted item number from the page

--- ebayItem.js
+++ ebayItem.js
@@ -1,1 +1,5 @@
-alert("Hello World");
+var item = document.getElementsByClassName('vi-xs');
+var regex = /([0-9]{12})/
+console.log(item[0].innerHTML);
+var match = regex.exec(item[0].innerHTML);
+console.log(match[0]);
